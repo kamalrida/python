26f76020b76dc7d97a5741830dea8f04b6623da6
kamalrida <kamalrida@gmail.com>
2013-10-26 14:19:27 +0200

    Update listComprehensions.py

--- listComprehensions.py
+++ listComprehensions.py
@@ -1,4 +1,20 @@
 [i%2 for i in range(10)]
+"""
+>>> [i%2 for i in range(10)]
+[0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+
+"""
+
 [i**.5 for i in range(10) if i%2]
+"""
+>>> [i**.5 for i in range(10) if i%2]
+[1.0, 1.7320508075688772, 2.23606797749979, 2.6457513110645907, 3.0]
+"""
+
 [j**2 for j in [i for i in range(10) if i%2] if j>2]
+"""
+>>> [j**2 for j in [i for i in range(10) if i%2] if j>2]
+[9, 25, 49, 81]
+"""
+
 [i**2 for i in range(10) if i%2 if i>2] ## same as previous
